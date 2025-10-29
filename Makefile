cfdd87f888ae52b26edf348ceea85d0c96844f19
j0shtran <hienjoshuatran@gmail.com>
2025-10-29 17:06:58 -0500

    Exercise 4

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CC = g++
 CFLAGS = -Wall 
 
-all: exercise1 exercise2 exercise3 
+all: exercise1 exercise2 exercise3 exercise4
  
 exercise1: exercise1.cpp 
 	$(CC) $(CFLAGS) exercise1.cpp -o exercise1 
@@ -9,5 +9,7 @@ exercise2: exercise2.cpp
 	$(CC) $(CFLAGS) exercise2.cpp -o exercise2  
 exercise3: exercise3.cpp
 	$(CC) $(CFLAGS) exercise3.cpp -o exercise3 
+exercise4: exercise4.cpp
+	$(CC) $(CFLAGS) exercise4.cpp -o exercise4 
 clean:
-	rm -rf *.o -f exercise1 exercise2 exercise3
+	rm -rf *.o -f exercise1 exercise2 exercise3 exercise4
---
+++ exercise4.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+  string filename;
+  int size;
+  int m_1[20][20];
+  int main = 0;
+  int secondary = 0;
+  std::cout << "Enter a filename: ";
+  std::cin >> filename;
+  ifstream myfile(filename);
+  myfile >> size;
+  for (int i=0; i<size; i++) {
+    for (int j = 0; j < size; j++) {
+      myfile >> m_1[i][j];
+    }
+  }
+  for (int i=0; i<size; i++) {
+    main += m_1[i][i];
+    secondary += m_1[i][size -i -1];
+  }
+ cout << "Result:\n";
+ cout << "Main sum: " << main << "\n";
+ cout << "Secondary sum: " << secondary << "\n";
+
+myfile.close();
+
+  return 0;
+}
